34925cafe5b5932c6afb22e0cf58a7c9d38eea29
Yash Raj <131522276+Yash-Raj3@users.noreply.github.com>
2025-09-17 21:11:24 +0500

    Add files via upload

---
+++ PF THEORY/PF theory Assignment/Question 1/Question-1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int transaction_no;
+    int transaction_amount;
+    char country[40];
+
+    printf("Enter the Transaction no in an hour:\t");
+    scanf("%d",&transaction_no);
+
+    printf("Enter Transaction Country:\t");
+    scanf("%s",country);
+
+    printf("Enter Transaction Amount:\t");
+    scanf("%d",&transaction_amount);
+
+    if(transaction_no > 3){
+        printf("Suspicious Transaction");
+    }
+    else if((strcmp(country,"Pakistan") && strcmp(country,"UAE"))!=0){
+        printf("Suspicious Transaction");
+    }
+    else if(transaction_amount > 5000){
+        printf("Suspicious Transaction");
+    }
+    else{
+        printf("Normal Transaction");
+    }
+    return 0;
+}
