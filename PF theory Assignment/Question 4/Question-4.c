c429b9fe166c49d96de53ed68a21753b474859fa
Yash Raj <131522276+Yash-Raj3@users.noreply.github.com>
2025-09-27 19:31:29 +0500

    Update Question-4.c

--- PF theory Assignment/Question 4/Question-4.c
+++ PF theory Assignment/Question 4/Question-4.c
@@ -18,7 +18,8 @@ int main(){
           (Card1==Card2 && Card2==Card5 && Card3==Card4 && Card3!=Card1)||
           (Card1==Card3 && Card3==Card4 && Card2==Card5 && Card2!=Card1)||
           (Card1==Card4 && Card4==Card5 && Card3==Card2 && Card3!=Card1)||
-          (Card1==Card3 && Card3==Card5 && Card2==Card4 && Card2!=Card1))
+          (Card1==Card3 && Card3==Card5 && Card2==Card4 && Card2!=Card1)||
+          (Card1==Card2 && Card2==Card4 && Card3==Card5 && Card5!=Card1))
          {
         printf("The Hand is full House !!");
           }else{
